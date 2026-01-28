07fb7210b9cbfcb3a409b8bc15cbbed8effec0a3
caiopenayo <112773671+caiopenayo@users.noreply.github.com>
2026-01-28 16:18:05 -0300

    Update datasets.py

--- data/datasets.py
+++ data/datasets.py
@@ -1,14 +1,9 @@
-# data/datasets.py
-
 import torch
 from torch.utils.data import random_split
 from torchvision import datasets, transforms
 
 
 def get_cifar100_transforms(img_size=160):
-    """
-    Retorna transforms de treino e teste compatíveis com ViT/DINO.
-    """
     train_transform = transforms.Compose([
         transforms.Resize(img_size),
         transforms.RandomHorizontalFlip(),
@@ -32,10 +27,6 @@ def get_cifar100_transforms(img_size=160):
 
 
 def get_cifar100(train_transform, test_transform, val_ratio=0.1, root="./data", seed=42):
-    """
-    Retorna:
-      train_subset, val_subset, test_dataset
-    """
     full_train = datasets.CIFAR100(
         root=root, train=True, download=True, transform=train_transform
     )
